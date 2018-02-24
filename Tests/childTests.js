11e4f82929fb8986956085d1c5f79ad48a5f5309
YuriiVasiuk <yuravasiuk@gmail.com>
2018-02-23 17:17:08 -0700

    Remade to do everything in pre-import; childTest.js is added.

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -1,12 +1,84 @@
 /*eslint-env node, es6*/
+/*eslint no-unused-vars:1*/
+/*eslint no-console:0, semi: 2*/
+
+/* This file still needs some work to do.
+   The logic for testing works, but the tap is not configured right at the higher level yet. 
+   I am testing each quiz for passing over to canvas some information in the description in the d2l.
+   The information has been collected from all 4 places (description, introduction, header, and footer), 
+   and the description specifies which one came from where.
+   Also, all descriptions for all quizzes are saved in the checkFiles folder in the html format.
+   I can open them in browser and check. */
 
 /* Dependencies */
 const tap = require('tap');
+const fs = require('fs');
+const path = require('path');
+const he = require('he');
+const cheerio = require('cheerio');
 
 function g1Tests(course, callback) {
-    // Tap tests for Gauntlet 1 go here
-    tap.pass('Success! Wheee! 1');
-    // tap.fail('YOLO');
+    var files = course.content.filter(function (file) {
+        return file.name.includes('quiz');
+    });
+
+    files.forEach(function (file) {
+        // getting the html content of the discription tag (the one that is sent to canvas) 
+        // and writing it into an html file
+        var tag = 'questestinterop>assessment>rubric>flow_mat>material>mattext';
+        var html_content = he.decode(file.dom(tag).text());
+        var new_file_name = file.name.replace('.xml', '.html');
+        fs.writeFileSync(path.join('.', 'checkFiles', new_file_name), html_content, 'utf8');
+
+        // make an array of elements to test against
+        var $ = cheerio.load(html_content);
+        var all_elements = [4];
+        $('h2').each(function (i, element) {
+            all_elements[i] = $(element).next('p').html();
+        });
+
+        // testing
+        console.log('\n--------------\nTesting ' + file.name + '\n-----------------');
+        var all_empty = 0,
+            all_filled = 4;
+        for (var i = 0; i < 4; i++) {
+            if (all_elements[i] === null) {
+                all_empty++;
+                all_filled--;
+            }
+        }
+        // #1 all empty
+        if (all_empty === 4) {
+            tap.pass();
+            console.log('test #1 -- all empty passed');
+        }
+        // #2 description
+        if (all_elements[0] !== null) {
+            tap.pass();
+            console.log('test #2 -- description passed');
+        }
+        // #3 introduction
+        if (all_elements[1] !== null) {
+            tap.pass();
+            console.log('test #3 -- introduction passed');
+        }
+        // #4 footer
+        if (all_elements[2] !== null) {
+            tap.pass();
+            console.log('test #4 -- footer passed');
+        }
+        // #5 header
+        if (all_elements[3] !== null) {
+            tap.pass();
+            console.log('test #5 -- header passed');
+        }
+        // #6 all filled
+        if (all_filled === 4) {
+            tap.pass();
+            console.log('test #6 -- all filled passed');
+        }
+        console.log('\n------------------------------');
+    });
     callback(null, course);
 }
 
@@ -29,20 +101,20 @@ function g4Tests(course, callback) {
 }
 
 module.exports = [
-        {
-            gauntlet: 1,
-            tests: g1Tests
+    {
+        gauntlet: 1,
+        tests: g1Tests
         },
-        {
-            gauntlet: 2,
-            tests: g2Tests
+    {
+        gauntlet: 2,
+        tests: g2Tests
         },
-        {
-            gauntlet: 3,
-            tests: g3Tests
+    {
+        gauntlet: 3,
+        tests: g3Tests
         },
-        {
-            gauntlet: 4,
-            tests: g4Tests
+    {
+        gauntlet: 4,
+        tests: g4Tests
         },
 ];
--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 /*eslint no-unused-vars:1*/
 /*eslint no-console:0, semi: 2*/
 
-/* The module checks 4 xml tags of each quiz file
-   and consoles log the report of the check. 
+/* All is done in the pre-import (synchronously).
+   In d2l there are 4 tags containing quiz information, but only the 1st one (Description) is sent to canvas
+   when the course is imported.  I am collecting the info from all 4 tags in d2l, 
+   add "Old ..." to explain where each one comes from, and put it all into the Description.
    For this module cheerio object is already loaded into dom,
    so all the cheerio calls are done with file.dom() */
 
@@ -21,7 +23,7 @@ module.exports = (course, stepCallback) => {
     // it is done in 3 steps
     function clearString(raw_string) {
         // step 1 
-        // just a raw cleaning using he lirary
+        // get it back to html format
         var cleared_string1 = he.decode(raw_string);
 
         // step 2
@@ -53,13 +55,23 @@ module.exports = (course, stepCallback) => {
         return cleared_string2;
     }
 
-    // all the jobe is done in this forEach()
     // this object will be filled with data for the log report
     var json_report = {};
-    // get all files with quizzes
+    // We check 4 xml elements of each quiz file:
+    var name = ['Description:', 'Introduction:', 'Page Header:', 'Page Footer:'];
+    var xml_tags = ['questestinterop>assessment>rubric>flow_mat>material>mattext',
+                          'd2l_2p0\\:intro_message',
+                          'material[label="page header"]>mattext',
+                          'material[label="page footer"]>mattext'];
+
+    // all the job is done in this forEach()
     course.content.forEach(function (file) {
+        // get all files with quizzes
         var regexp = /quiz[\s\S]*\.xml$/;
         if (regexp.test(file.name)) {
+            // the content collector for remaking the discription tag
+            var content_collector = '';
+            // will be used for checking and clearing the tag's content
             var to_check_raw = '';
             var to_check_cleared = '';
 
@@ -67,34 +79,34 @@ module.exports = (course, stepCallback) => {
             var quiz_name = file.dom('assessment[d2l_2p0\\:id]').attr('title');
             json_report['Quiz name: '] = quiz_name;
 
-
-            // We check 4 xml elements of each quiz file:
-            var name = ['Description:', 'Introduction:', 'Page Header:', 'Page Footer:'];
-            var xml_tag = ['mattext[d2l_2p0\\:isdisplayed="yes"]',
-                          'd2l_2p0\\:intro_message',
-                          'material[label="page header"]>mattext',
-                          'material[label="page footer"]>mattext'];
-
             for (var i = 0; i < name.length; i++) {
+                var temp = he.encode('<h2 style="color:red;font-size: 24px;"><strong>Old ' +
+                    name[i] + '</strong></h2>');
+                content_collector += temp;
                 var new_information = name[i];
-                // this line fishes out the needed tag and gets the text of it
-                to_check_raw = file.dom(xml_tag[i]).text();
+                // this line fishes out the needed tag and gets the text out of it
+                to_check_raw = file.dom(xml_tags[i]).text();
                 if (to_check_raw !== '') {
+                    content_collector += to_check_raw;
                     to_check_cleared = clearString(to_check_raw);
                     // fill up the Descriptions
                     json_report[new_information] = to_check_cleared;
                 }
-
                 to_check_raw = '';
                 to_check_cleared = '';
             }
+
+            // finally, replace the old description with the information collected from 4 tags
+            // the description is sent automatically from d2l to canvas
+            file.dom(xml_tags[0]).text(content_collector, {
+                'useNamedReferences': true
+            });
+
             /* Used to log successful actions */
             course.log('Descriptions for the quizz', json_report);
         }
         json_report = {};
     });
 
-    /* You should never call the stepCallback with an error. We want the
-    whole program to run when testing so we can catch all existing errors */
     stepCallback(null, course);
 };
