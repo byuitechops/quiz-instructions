33817032542b1518394a5c804180cc7882df2dfc
Danverde <gre12014@byui.edu>
2018-03-12 10:19:24 -0600

    removed module report. switched vars to camelCase

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-/*eslint-env node, es6*/
-/*eslint no-unused-vars:1*/
-/*eslint no-console:0, semi: 2*/
-
 /* All is done in the pre-import (synchronously).
    In d2l there are 4 tags containing quiz information, but only the 1st one (Description) is sent to canvas
    when the course is imported.  I am collecting the info from all 4 tags in d2l, 
@@ -9,60 +5,56 @@
    For this module cheerio object is already loaded into dom,
    so all the cheerio calls are done with file.dom() */
 
-var he = require('he');
+const he = require('he');
 
 /* View available course object functions */
 // https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
 
 module.exports = (course, stepCallback) => {
-    /* Create the module report so that we can access it later as needed.
-    This MUST be done at the beginning of each child module. */
-    course.addModuleReport('quiz-instructions');
-
     // this axillary function will clear the passed string and return only the text
     // it is done in 3 steps
     function clearString(raw_string) {
         // step 1 
         // get it back to html format
-        var cleared_string1 = he.decode(raw_string);
+        var clearedString1 = he.decode(raw_string);
 
         // step 2
         // clear everything like <...> (including the angular brackets)
-        var cleared_string2 = '';
+        var clearedString2 = '';
         var add = true;
-        for (var i = 0; i < cleared_string1.length; i++) {
-            if (cleared_string1.charAt(i) === '<' && add === true) {
+        for (var i = 0; i < clearedString1.length; i++) {
+            if (clearedString1.charAt(i) === '<' && add === true) {
                 add = false;
             }
-            if (cleared_string1.charAt(i) === '>' && add === false) {
+            if (clearedString1.charAt(i) === '>' && add === false) {
                 add = true;
             }
-            if (add === true && cleared_string1.charAt(i) !== '>') {
-                cleared_string2 += cleared_string1.charAt(i);
+            if (add === true && clearedString1.charAt(i) !== '>') {
+                clearedString2 += clearedString1.charAt(i);
             }
         }
 
         // step 3
         // the final refinement
-        var words_to_remove = ['&nbsp;', '&ldquo;', '&rdquo;'];
-        for (var k = 0; k < words_to_remove.length; k++) {
-            var remove = cleared_string2.includes(words_to_remove[k]);
+        var wordsToRemove = ['&nbsp;', '&ldquo;', '&rdquo;'];
+        for (var k = 0; k < wordsToRemove.length; k++) {
+            var remove = clearedString2.includes(wordsToRemove[k]);
             while (remove) {
-                cleared_string2 = cleared_string2.replace(words_to_remove[k], '');
-                remove = cleared_string2.includes(words_to_remove[k]);
+                clearedString2 = clearedString2.replace(wordsToRemove[k], '');
+                remove = clearedString2.includes(wordsToRemove[k]);
             }
         }
-        return cleared_string2;
+        return clearedString2;
     }
 
     // this object will be filled with data for the log report
-    var json_report = {};
+    var jsonReport = {};
     // We check 4 xml elements of each quiz file:
     var name = ['Description:', 'Introduction:', 'Page Header:', 'Page Footer:'];
     var xml_tags = ['questestinterop>assessment>rubric>flow_mat>material>mattext',
-                          'd2l_2p0\\:intro_message',
-                          'material[label="page header"]>mattext',
-                          'material[label="page footer"]>mattext'];
+        'd2l_2p0\\:intro_message',
+        'material[label="page header"]>mattext',
+        'material[label="page footer"]>mattext'];
 
     // all the job is done in this forEach()
     course.content.forEach(function (file) {
@@ -70,42 +62,42 @@ module.exports = (course, stepCallback) => {
         var regexp = /quiz[\s\S]*\.xml$/;
         if (regexp.test(file.name)) {
             // the content collector for remaking the discription tag
-            var content_collector = '';
+            var contentCollector = '';
             // will be used for checking and clearing the tag's content
-            var to_check_raw = '';
-            var to_check_cleared = '';
+            var toCheckRaw = '';
+            var toCheckCleared = '';
 
             // fill up the quiz names for the module report
-            var quiz_name = file.dom('assessment[d2l_2p0\\:id]').attr('title');
-            json_report['Quiz name: '] = quiz_name;
+            var quizName = file.dom('assessment[d2l_2p0\\:id]').attr('title');
+            jsonReport['Quiz name: '] = quizName;
 
             for (var i = 0; i < name.length; i++) {
                 var temp = he.encode('<h2 style="color:red;font-size: 24px;"><strong>Old ' +
                     name[i] + '</strong></h2>');
-                content_collector += temp;
+                contentCollector += temp;
                 var new_information = name[i];
                 // this line fishes out the needed tag and gets the text out of it
-                to_check_raw = file.dom(xml_tags[i]).text();
-                if (to_check_raw !== '') {
-                    content_collector += to_check_raw;
-                    to_check_cleared = clearString(to_check_raw);
+                toCheckRaw = file.dom(xml_tags[i]).text();
+                if (toCheckRaw !== '') {
+                    contentCollector += toCheckRaw;
+                    toCheckCleared = clearString(toCheckRaw);
                     // fill up the Descriptions
-                    json_report[new_information] = to_check_cleared;
+                    jsonReport[new_information] = toCheckCleared;
                 }
-                to_check_raw = '';
-                to_check_cleared = '';
+                toCheckRaw = '';
+                toCheckCleared = '';
             }
 
             // finally, replace the old description with the information collected from 4 tags
             // the description is sent automatically from d2l to canvas
-            file.dom(xml_tags[0]).text(content_collector, {
+            file.dom(xml_tags[0]).text(contentCollector, {
                 'useNamedReferences': true
             });
 
             /* Used to log successful actions */
-            course.log('Descriptions for the quizz', json_report);
+            course.log('Descriptions for the quiz', jsonReport);
         }
-        json_report = {};
+        jsonReport = {};
     });
 
     stepCallback(null, course);
