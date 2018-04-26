8c10ba5219558ed887d7e3be9946ead5ba3a9a19
Seth Childers <sethchilders92@gmail.com>
2018-04-26 09:19:56 -0600

    Reset

---
+++ README.md
@@ -0,0 +1,47 @@
+# Quiz Instructions
+### *Package Name*: quiz-instructions
+### *Child Type*: preimport
+### *Platform*: all
+### *Required*: Required
+
+This child module is built to be used by the Brigham Young University - Idaho D2L to Canvas Conversion Tool. It utilizes the standard `module.exports => (course, stepCallback)` signature and uses the Conversion Tool's standard logging functions. You can view extended documentation [Here](https://github.com/byuitechops/d2l-to-canvas-conversion-tool/tree/master/documentation).
+
+## Purpose
+
+The module makes sure that descriprion and instructions for every quiz in the course will be trasfered from d2l to canvas.
+
+## How to Install
+
+```
+npm install set-syllabus
+```
+
+## Run Requirements
+
+course.content
+
+course.log
+
+## Options
+
+N/A
+
+## Outputs
+
+The course changed in putting contents from ['Description', 'Introduction', 'Page Header', 'Page Footer'] into the Description.
+The module does not add anything to `course.info` or anywhere else on the course object.
+
+## Process
+
+1. Check 4 xml elements of each quiz file for any content 
+2. If there is, put the content into the content collector
+3. Replace the content in the Description with the content from the content collector
+
+## Log Categories
+
+course.log('Descriptions for the quiz', jsonReport);
+
+## Requirements
+
+Canvas has only Description, but d2l has quiz info in 4 places:['Description', 'Introduction', 'Page Header', 'Page Footer']. The module locates all the different places the quiz instructions and descriptions are and put them all into the Description.  In the process of course import the Description will be ransfered from d2l to Canvas. 
+The module returns the course with the updated Description for each quiz.
---
+++ main.js
@@ -0,0 +1,93 @@
+/* All is done in the pre-import (synchronously).
+   In d2l there are 4 tags containing quiz information, but only the 1st one (Description) is sent to canvas
+   when the course is imported.  I am collecting the info from all 4 tags in d2l, 
+   add "Old ..." to explain where each one comes from, and put it all into the Description.
+   For this module cheerio object is already loaded into dom,
+   so all the cheerio calls are done with file.dom() */
+
+const he = require('he');
+
+module.exports = (course, stepCallback) => {
+    // this axillary function will clear the passed string and return only the text
+    // it is done in 3 steps
+    function clearString(rawString) {
+        // step 1 
+        // get it back to html format
+        var clearedString1 = he.decode(rawString);
+
+        // step 2
+        // clear everything like <...> (including the angular brackets)
+        clearedString1 = clearedString1.replace(/<[\s\S]*?.>/g, '');
+
+        // step 3
+        // the final refinement
+        var wordsToRemove = ['&nbsp;', '&ldquo;', '&rdquo;'];
+
+        var regEx;
+        wordsToRemove.forEach(wordToRemove => {
+            regEx = new RegExp(wordToRemove, 'g'); // convert string to regular expression
+            clearedString1 = clearedString1.replace(regEx, '');
+        });
+
+        return clearedString1;
+    }
+
+    // this object will be filled with data for the log report
+    var jsonReport = {};
+    // We check 4 xml elements of each quiz file:
+    var name = ['Description', 'Introduction', 'Page Header', 'Page Footer'];
+    var xml_tags = ['questestinterop>assessment>rubric>flow_mat>material>mattext',
+        'd2l_2p0\\:intro_message',
+        'material[label="page header"]>mattext',
+        'material[label="page footer"]>mattext'
+    ];
+
+    // all the job is done in this forEach()
+    course.content.forEach(function (file) {
+        // get all files with quizzes
+        var regexp = /quiz[\s\S]*\.xml$/;
+        if (regexp.test(file.name)) {
+            // the content collector for remaking the discription tag
+            var contentCollector = '';
+            // will be used for checking and clearing the tag's content
+            var toCheckRaw = '';
+            var toCheckCleared = '';
+
+            // fill up the quiz names for the module report
+            var quizName = file.dom('assessment[d2l_2p0\\:id]').attr('title');
+            jsonReport['Quiz name'] = quizName;
+
+            for (var i = 0; i < name.length; i++) {
+                var temp = he.encode('<h2 style="color:red;font-size: 24px;"><strong>Old ' +
+                    name[i] + '</strong></h2>');
+                contentCollector += temp;
+                var newInformation = name[i];
+                // this line fishes out the needed tag and gets the text out of it
+                toCheckRaw = file.dom(xml_tags[i]).text();
+                if (toCheckRaw !== '') {
+                    contentCollector += toCheckRaw;
+                    toCheckCleared = clearString(toCheckRaw);
+                    // fill up the Descriptions
+                    jsonReport[newInformation] = toCheckCleared;
+                }
+                toCheckRaw = '';
+                toCheckCleared = '';
+            }
+
+            // finally, replace the old description with the information collected from 4 tags
+            // the description is sent automatically from d2l to canvas
+            file.dom(xml_tags[0]).text(contentCollector, {
+                'useNamedReferences': true
+            });
+            file.dom(xml_tags[1]).text('');
+            file.dom(xml_tags[2]).text('');
+            file.dom(xml_tags[3]).text('');
+
+            /* Used to log successful actions */
+            course.log('Descriptions for the quiz', jsonReport);
+        }
+        jsonReport = {};
+    });
+
+    stepCallback(null, course);
+};
